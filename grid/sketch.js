756efaa031ab6a6108268b666ddcba457176b570
DanYuz1 <dan.yuzbashev@gmail.com>
2025-10-28 09:52:44 -0600

    stuff

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,24 +1,28 @@
 // Grid Demo
-// 2d arrays
+// learning 2d arrays
 
-let theGrid = [
-  [1,0,1,0], 
-  [0,0,1,1],
-  [1,1,0,0],
-  [0,1,0,1]
-];
+//if you're going to hard code the grid, use this!
+// let theGrid = [[1, 0, 1, 0],
+//                [0, 0, 1, 1],
+//                [1, 1, 0, 0],
+//                [0, 1, 0, 1]];
+// const SQUARE_DIMENSIONS = theGrid.length;
+
+//if you're going to randomize the grid, use this!
+let theGrid;
+const SQUARE_DIMENSIONS = 10;
 
 let cellSize;
-let SQUARE_DIMENSIONS =  theGrid.length;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  if(width < height) {
+  if (width < height) {
     cellSize = width/SQUARE_DIMENSIONS;
   }
-  else{
+  else {
     cellSize = height/SQUARE_DIMENSIONS;
   }
+  theGrid = generateRandomGrid(SQUARE_DIMENSIONS, SQUARE_DIMENSIONS);
 }
 
 function draw() {
@@ -26,14 +30,13 @@ function draw() {
   showGrid();
 }
 
-
-function showGrid(){
-  for (let y = 0; y > SQUARE_DIMENSIONS; y++){
-    for (let x = 0; x < SQUARE_DIMENSIONS; x++){
-      if (theGrid[y][x] === 1){
+function showGrid() {
+  for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
+    for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
+      if (theGrid[y][x] === 1) {
         fill("black");
       }
-      else if (theGrid[y][x] === 0){
+      else if (theGrid[y][x] === 0) {
         fill("white");
       }
       square(x * cellSize, y * cellSize, cellSize);
@@ -41,6 +44,35 @@ function showGrid(){
   }
 }
 
+
 function mousePressed() {
-  
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  if (theGrid[y][x] === 1) {
+    theGrid[y][x] = 0;
+  }
+  else if (theGrid[y][x] === 0) {
+    theGrid[y][x] = 1;
+  }
+}
+
+function generateRandomGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(0);
+      }
+      else {
+        newGrid[y].push(1);
+      }
+    }
+  }
+  return newGrid;
 }
